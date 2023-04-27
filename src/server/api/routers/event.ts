8310acec123e885e40eb1382b36b9c48ebf332ee
JuankGF <juankgonzalezf@gmail.com>
2023-04-27 11:39:58 -0400

    Add event router

---
+++ src/server/api/routers/event.ts
@@ -0,0 +1,132 @@
+import { z } from "zod";
+
+import {
+  createTRPCRouter,
+  publicProcedure,
+  protectedProcedure,
+} from "~/server/api/trpc";
+
+export const eventRouter = createTRPCRouter({
+  getAllPublic: publicProcedure.query(({ ctx }) => {
+    return ctx.prisma.event.findMany({
+      where: {
+        isPublic: true,
+      },
+    });
+  }),
+
+  getAll: protectedProcedure.query(({ ctx }) => {
+    return ctx.prisma.event.findMany();
+  }),
+
+  getAllPrivate: protectedProcedure.query(({ ctx }) => {
+    return ctx.prisma.event.findMany({
+      where: {
+        isPublic: false,
+      },
+    });
+  }),
+
+  create: protectedProcedure
+    .input(
+      z.object({
+        name: z.string(),
+        description: z.string(),
+        type: z.string(),
+        date: z.date(),
+        location: z.string(),
+        address: z.string().optional(),
+        isPublic: z.boolean().optional(),
+        media: z
+          .object({
+            name: z.string(),
+            path: z.string(),
+            extension: z.string(),
+          })
+          .optional(),
+      })
+    )
+    .mutation(({ ctx, input }) => {
+      return ctx.prisma.event.create({
+        data: {
+          name: input.name,
+          description: input.description,
+          type: input.type,
+          date: input.date,
+          address: input.address,
+          location: input.location,
+          isPublic: input.isPublic,
+          media: input.media
+            ? {
+                create: {
+                  name: input.media.name,
+                  path: input.media.path,
+                  extension: input.media.extension,
+                },
+              }
+            : undefined,
+        },
+      });
+    }),
+
+  update: protectedProcedure
+    .input(
+      z.object({
+        id: z.string(),
+        name: z.string().optional(),
+        description: z.string().optional(),
+        type: z.string().optional(),
+        date: z.date().optional(),
+        location: z.string().optional(),
+        address: z.string().optional(),
+        isPublic: z.boolean().optional(),
+        media: z
+          .object({
+            name: z.string(),
+            path: z.string(),
+            extension: z.string(),
+          })
+          .optional(),
+      })
+    )
+    .mutation(({ ctx, input }) => {
+      return ctx.prisma.event.update({
+        where: {
+          id: input.id,
+        },
+        data: {
+          name: input.name,
+          description: input.description,
+          type: input.type,
+          date: input.date,
+          address: input.address,
+          location: input.location,
+          isPublic: input.isPublic,
+          media: input.media
+            ? {
+                connectOrCreate: {
+                  where: {
+                    path: input.media.path,
+                  },
+                  create: {
+                    name: input.media.name,
+                    path: input.media.path,
+                    extension: input.media.extension,
+                  },
+                },
+              }
+            : undefined,
+        },
+      });
+    }),
+
+  delete: protectedProcedure
+    .input(z.object({ id: z.string() }))
+    .mutation(({ ctx, input }) => {
+      return ctx.prisma.media.delete({
+        where: {
+          id: input.id,
+        },
+      });
+    }),
+});

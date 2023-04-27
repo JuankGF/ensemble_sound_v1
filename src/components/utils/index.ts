4752f976c437e5c900fc391a41bff502ed01706b
JuankGF <juankgonzalezf@gmail.com>
2023-04-27 16:19:44 -0400

    Update exports

--- src/components/utils/index.ts
+++ src/components/utils/index.ts
@@ -1,5 +1,6 @@
 import Hero from "./Hero";
 import HeroSection from "./HeroSection";
 import ImageCarousel from "./ImageCarousel";
+import Button from "./Button";
 
-export { Hero, HeroSection, ImageCarousel };
+export { Hero, HeroSection, ImageCarousel, Button };

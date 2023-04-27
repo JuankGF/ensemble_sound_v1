d82a2bee64613f63d76ef2fa8d7d209f610150e7
JuankGF <juankgonzalezf@gmail.com>
2023-04-27 16:19:12 -0400

    Added button component and image carousel Daisy UI adaptation

---
+++ src/components/utils/Button.tsx
@@ -0,0 +1,55 @@
+import React, { type PropsWithChildren } from "react";
+
+export type ButtonVariant =
+  | "primary"
+  | "secondary"
+  | "accent"
+  | "ghost"
+  | "link"
+  | "info"
+  | "success"
+  | "warning"
+  | "error";
+
+type ButtonProps = {
+  className?: string;
+  disabled?: boolean;
+  glass?: boolean;
+  loading?: boolean;
+  outline?: boolean;
+  size?: "lg" | "sm" | "xs";
+  shape?: "square" | "circle";
+  variant?: ButtonVariant;
+  onClick: () => void;
+};
+
+export default function Button({
+  className,
+  children,
+  disabled,
+  glass,
+  loading,
+  outline,
+  onClick,
+  size,
+  shape,
+  variant = "primary",
+}: PropsWithChildren<ButtonProps>) {
+  const shapeClass = shape ? `btn-${shape}` : "";
+  const sizeClass = size ? `btn-${size}` : "";
+  const outlineClass = outline ? `btn-outline` : "";
+  const loadingState = loading ? `btn-loading` : "";
+  const glassClass = glass ? `btn-glass` : "";
+
+  return (
+    <button
+      className={`btn gap-2 ${
+        className ?? ""
+      } btn-${variant} ${shapeClass} ${sizeClass} ${outlineClass} ${loadingState} ${glassClass}`}
+      disabled={disabled}
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  );
+}
--- src/components/utils/ImageCarousel.tsx
+++ src/components/utils/ImageCarousel.tsx
@@ -1,29 +1,47 @@
 import Image, { type StaticImageData } from "next/image";
 import React from "react";
-import {
-  Carousel,
-  type CarouselProps as CarouselOriginalProps,
-} from "react-bootstrap";
 
 type CarouselProps = {
   image_sources: string[] | StaticImageData[];
-} & CarouselOriginalProps;
+  indicators?: boolean;
+};
 
 export default function ImageCarousel({
   image_sources,
-  ...props
+  indicators = true,
 }: CarouselProps) {
   return (
-    <Carousel fade {...props}>
-      {image_sources.map((image, index) => (
-        <Carousel.Item key={`${image.toString()}_${index}`}>
-          <Image
-            className="d-block w-100 rounded-sm carousel-img"
-            src={image}
-            alt={`${index + 1} slide`}
-          />
-        </Carousel.Item>
-      ))}
-    </Carousel>
+    <>
+      <div className="carousel-center carousel rounded-box max-w-md space-x-4 bg-neutral p-4">
+        {image_sources.map((image, index) => (
+          <div
+            id={`item${index + 1}`}
+            className="carousel-item"
+            key={`${image.toString()}-${index}`}
+          >
+            <div className="carousel-item">
+              <Image
+                className="carousel-img rounded-box"
+                src={image}
+                alt={`${index + 1} slide`}
+              />
+            </div>
+          </div>
+        ))}
+      </div>
+      {indicators && (
+        <div className="flex w-full justify-center gap-2 py-2">
+          {image_sources.map((image, index) => (
+            <a
+              href={`#item${index + 1}`}
+              className="btn-xs btn"
+              key={`${image.toString()}-${index}`}
+            >
+              {index + 1}
+            </a>
+          ))}
+        </div>
+      )}
+    </>
   );
 }

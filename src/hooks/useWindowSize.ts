de346acdbbf4e0fa04be2d14481b6827725f1aad
JuankGF <juankgonzalezf@gmail.com>
2023-04-27 15:08:01 -0400

    Fix hook for client side only usage

--- src/hooks/useWindowSize.ts
+++ src/hooks/useWindowSize.ts
@@ -1,32 +1,34 @@
 import { useEffect, useState } from "react";
 
 type Size = {
-  width: number;
-  height: number;
+  width: number | undefined;
+  height: number | undefined;
 };
 
 const useWindowSize = () => {
   const [windowSize, setWindowSize] = useState<Size>({
-    width: window.innerWidth,
-    height: window.innerHeight,
+    width: undefined,
+    height: undefined,
   });
 
   useEffect(() => {
-    function handleResize() {
-      setWindowSize({
-        width: window.innerWidth,
-        height: window.innerHeight,
-      });
-    }
-    window.addEventListener("resize", handleResize);
+    if (typeof window !== "undefined") {
+      function handleResize() {
+        setWindowSize({
+          width: window.innerWidth,
+          height: window.innerHeight,
+        });
+      }
+      window.addEventListener("resize", handleResize);
 
-    handleResize();
-    return () => window.removeEventListener("resize", handleResize);
+      handleResize();
+      return () => window.removeEventListener("resize", handleResize);
+    }
   }, []);
   return {
     windowSize,
-    isSmallScreen: windowSize.width <= 820,
-    isMobile: windowSize.width <= 480,
+    isSmallScreen: (windowSize.width ?? 0) <= 820,
+    isMobile: (windowSize.width ?? 0) <= 480,
   };
 };
 

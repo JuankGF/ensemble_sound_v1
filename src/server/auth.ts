e77397217cf9191e7a1ab8c8703becbd5db5ea6c
JuankGF <juankgonzalezf@gmail.com>
2023-04-27 11:48:05 -0400

    Update Account model for GitHub refresh_token_expires_in required field

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -8,6 +8,7 @@ import GitHubProvider from "next-auth/providers/github";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { env } from "~/env.mjs";
 import { prisma } from "~/server/db";
+import { type Role } from "@prisma/client";
 
 /**
  * Module augmentation for `next-auth` types. Allows us to add custom properties to the `session`
@@ -20,7 +21,7 @@ declare module "next-auth" {
     user: {
       id: string;
       // ...other properties
-      // role: UserRole;
+      role: Role;
     } & DefaultSession["user"];
   }
 

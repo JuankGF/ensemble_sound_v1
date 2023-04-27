4d2689e8c601130d3ef83d8be5e60d4821f5d1c0
JuankGF <juankgonzalezf@gmail.com>
2023-04-27 17:09:40 -0400

    Add hero section to main page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,11 @@
-import styles from "./index.module.css";
+import styles from "./index.module.scss";
 import { type NextPage } from "next";
 import Head from "next/head";
 import Link from "next/link";
 import { signIn, signOut, useSession } from "next-auth/react";
 
 import { api } from "~/utils/api";
+import { HeroSection } from "~/components/utils";
 
 const Home: NextPage = () => {
   const hello = api.example.hello.useQuery({ text: "from tRPC" });
@@ -14,7 +15,7 @@ const Home: NextPage = () => {
       <Head>
         <title>Ensemble Sound</title>
         <meta charSet="utf-8" />
-        <link rel="icon" href="%PUBLIC_URL%/favicon.ico" />
+        <link rel="icon" href="/favicon.ico" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="theme-color" content="#FFFFF" />
         <meta name="author" content="Ensemble Sound" />
@@ -22,50 +23,23 @@ const Home: NextPage = () => {
           name="description"
           content="Home web of Ensemble Sound audio and sound company."
         />
-        <link rel="apple-touch-icon" href="%PUBLIC_URL%/logo192.png" />
-        <link rel="manifest" href="%PUBLIC_URL%/manifest.json" />
+        <link rel="apple-touch-icon" href="/logo192.png" />
+        <link rel="manifest" href="/manifest.json" />
         <meta name="og:title" content="Ensemble Sound" />
         <meta
           name="og:description"
           content="Home web of Ensemble Sound audio and sound company"
         />
-        <meta name="og:image" content="%PUBLIC_URL%/favicon.ico" />
+        <meta name="og:image" content="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        <div className={styles.container}>
-          <h1 className={styles.title}>
-            Create <span className={styles.pinkSpan}>T3</span> App
-          </h1>
-          <div className={styles.cardRow}>
-            <Link
-              className={styles.card}
-              href="https://create.t3.gg/en/usage/first-steps"
-              target="_blank"
-            >
-              <h3 className={styles.cardTitle}>First Steps →</h3>
-              <div className={styles.cardText}>
-                Just the basics - Everything you need to know to set up your
-                database and authentication.
-              </div>
-            </Link>
-            <Link
-              className={styles.card}
-              href="https://create.t3.gg/en/introduction"
-              target="_blank"
-            >
-              <h3 className={styles.cardTitle}>Documentation →</h3>
-              <div className={styles.cardText}>
-                Learn more about Create T3 App, the libraries it uses, and how
-                to deploy it.
-              </div>
-            </Link>
-          </div>
-          <div className={styles.showcaseContainer}>
-            <p className={styles.showcaseText}>
-              {hello.data ? hello.data.greeting : "Loading tRPC query..."}
-            </p>
-            <AuthShowcase />
-          </div>
+        <HeroSection />
+
+        <div className={styles.showcaseContainer}>
+          <p className={styles.showcaseText}>
+            {hello.data ? hello.data.greeting : "Loading tRPC query..."}
+          </p>
+          <AuthShowcase />
         </div>
       </main>
     </>

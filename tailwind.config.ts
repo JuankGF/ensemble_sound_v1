0e54ba2010e615dbd38673c64b52bc4db8b95fa2
JuankGF <juankgonzalezf@gmail.com>
2023-04-27 18:09:16 -0400

    Update theme and colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import { type Config } from "tailwindcss";
+import colors from "tailwindcss/colors";
 
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
@@ -6,4 +7,19 @@ export default {
     extend: {},
   },
   plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: colors.purple[900],
+          secondary: colors.purple[400],
+          accent: colors.green[400],
+          neutral: "#3d4451",
+          "base-100": "#ffffff",
+        },
+      },
+      "emerald",
+      "dark",
+    ],
+  },
 } satisfies Config;

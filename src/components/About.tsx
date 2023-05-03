c76eb38ed0e11005405009a05f70a55d5ea4eda2
JuankGF <juankgonzalezf@gmail.com>
2023-05-03 11:30:45 -0400

    Add about images with variable displays throghout breakpoints

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -3,32 +3,48 @@ import Image from "next/image";
 
 import { Hero } from "./utils";
 import banner1 from "~/assets/banners/session1.jpeg";
-import banner2 from "~/assets/banners/session2.jpeg";
+import banner2 from "~/assets/banners/session2.jpg";
 
 export default function About() {
   return (
     <Hero
       text="Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.
       Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit."
-      className="section bg-base-100"
-      text_class="text-primary-content"
-      title_class="text-primary"
+      className="section bg-white px-3 md:px-10 lg:px-16"
+      text_class="text-primary"
+      title_class="text-primary text-3xl md:text-center lg:text-left"
     >
-      <div className="container relative w-full">
-        <Image
-          alt="banner-1"
-          src={banner1}
-          width={100}
-          height={100}
-          className=""
-        />
-        <Image
-          alt="banner-2"
-          src={banner2}
-          width={100}
-          height={100}
-          className=""
-        />
+      <div className="relative hidden flex-1 flex-row md:flex">
+        <div className="carousel-center carousel rounded-box h-96 min-w-max lg:carousel-vertical">
+          <div id="banner1" className="carousel-item h-full">
+            <Image
+              alt="banner-1"
+              src={banner1}
+              width={200}
+              height={200}
+              className="w-56 object-cover"
+            />
+          </div>
+          <div id="banner2" className="carousel-item h-full">
+            <Image
+              alt="banner-2"
+              src={banner2}
+              width={200}
+              height={200}
+              className="w-56 object-cover"
+            />
+          </div>
+        </div>
+        <div className="ml-2 hidden h-96 w-min items-center justify-center gap-2 py-2 lg:flex lg:flex-col">
+          <a
+            href="#banner1"
+            className="h-6 w-2 rounded bg-primary hover:bg-secondary-focus"
+          ></a>
+          <a
+            href="#banner2"
+            className="h-6 w-2 rounded bg-primary hover:bg-secondary-focus"
+          ></a>
+        </div>
       </div>
     </Hero>
   );

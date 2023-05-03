f256921d3c751f1955db48fce1a3995f8cc40728
JuankGF <juankgonzalezf@gmail.com>
2023-05-03 11:58:23 -0400

    Use route hash to style active links

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -4,8 +4,12 @@ import Image from "next/image";
 import { Hero } from "./utils";
 import banner1 from "~/assets/banners/session1.jpeg";
 import banner2 from "~/assets/banners/session2.jpg";
+import { useRouteHash } from "~/hooks/useRouteHash";
+import Link from "next/link";
 
 export default function About() {
+  const { isActiveRoute } = useRouteHash();
+
   return (
     <Hero
       text="Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.
@@ -16,7 +20,7 @@ export default function About() {
     >
       <div className="relative hidden flex-1 flex-row md:flex">
         <div className="carousel-center carousel rounded-box h-96 min-w-max lg:carousel-vertical">
-          <div id="banner1" className="carousel-item h-full">
+          <div id="banner-1" className="carousel-item h-full">
             <Image
               alt="banner-1"
               src={banner1}
@@ -25,7 +29,7 @@ export default function About() {
               className="w-56 object-cover"
             />
           </div>
-          <div id="banner2" className="carousel-item h-full">
+          <div id="banner-2" className="carousel-item h-full">
             <Image
               alt="banner-2"
               src={banner2}
@@ -35,15 +39,23 @@ export default function About() {
             />
           </div>
         </div>
-        <div className="ml-2 hidden h-96 w-min items-center justify-center gap-2 py-2 lg:flex lg:flex-col">
-          <a
-            href="#banner1"
-            className="h-6 w-2 rounded bg-primary hover:bg-secondary-focus"
-          ></a>
-          <a
-            href="#banner2"
-            className="h-6 w-2 rounded bg-primary hover:bg-secondary-focus"
-          ></a>
+        <div className="hidden h-96 w-min -translate-x-4 items-center justify-center gap-2 py-2 lg:flex lg:flex-col">
+          <Link
+            href="#banner-1"
+            className={`h-6 w-2 rounded hover:bg-secondary-focus ${
+              isActiveRoute("/#banner-1")
+                ? "bg-secondary-focus"
+                : "bg-secondary"
+            }`}
+          ></Link>
+          <Link
+            href="#banner-2"
+            className={`h-6 w-2 rounded hover:bg-secondary-focus ${
+              isActiveRoute("/#banner-2")
+                ? "bg-secondary-focus"
+                : "bg-secondary"
+            }`}
+          ></Link>
         </div>
       </div>
     </Hero>

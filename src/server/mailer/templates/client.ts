b69f53d18bc01dfe0da049855a8e2650449dffa4
JuankGF <juankgonzalezf@gmail.com>
2023-05-16 09:40:47 -0400

    Update client templates

--- src/server/mailer/templates/client.ts
+++ src/server/mailer/templates/client.ts
@@ -1,7 +1,7 @@
 export const clientHTML = `<h1>We Received Your Message!</h1>
 <p>Dear %NAME%:</p>
 <p>
-  Thank you for inquiring. We just wanted to let you know that we've received
+  Thank you for contact us. We just wanted to let you know that we've received
   your message and will be responding soon.
 </p>
 <p>Have a great day!</p>

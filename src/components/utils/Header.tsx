a8ee31cba320cdd0409e9690343076727bda576e
JuankGF <juankgonzalezf@gmail.com>
2023-05-18 14:28:27 -0400

    comment unused links

--- src/components/utils/Header.tsx
+++ src/components/utils/Header.tsx
@@ -122,12 +122,12 @@ export default function Header() {
                 tabIndex={0}
                 className="dropdown-content menu rounded-box menu-compact mt-3 w-52 bg-base-100 p-2 shadow"
               >
-                <li>
+                {/* <li>
                   <a className="justify-between">
                     Profile
                     <span className="badge">New</span>
                   </a>
-                </li>
+                </li> */}
                 <li>
                   <a onClick={() => void signOut()}>Logout</a>
                 </li>

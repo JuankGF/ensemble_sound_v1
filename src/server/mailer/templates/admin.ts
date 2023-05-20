6cbcc83bc95280a74d292191ede0bb042795afc8
JuankGF <juankgonzalezf@gmail.com>
2023-05-20 06:33:40 -0400

    Add bold text to email templates

--- src/server/mailer/templates/admin.ts
+++ src/server/mailer/templates/admin.ts
@@ -7,6 +7,6 @@ export const adminHTML = `<h1>New Request Received From Ensemble Sound Website</
 <a href="tel:%PHONE%">Phone: %PHONE%</a>
 <p>Address: %ADDRESS%</p>
 <p>Event Date: %BOOKING_DATE%</p>
-<p>Event Type: %TYPE%</p>
+<p>Event Type: <b>%TYPE%</b></p>
 <p>Message:</p>
 <p>%MESSAGE%</p>`;
--- src/server/mailer/templates/client.ts
+++ src/server/mailer/templates/client.ts
@@ -1,8 +1,8 @@
 export const clientHTML = `<h1>We Received Your Message!</h1>
-<p>Dear %NAME%:</p>
+<h1>Dear <b>%NAME%</b>:</h1>
 <p>
   Thank you for contact us. We just wanted to let you know that we've received
   your message and will be responding soon.
 </p>
 <p>Have a great day!</p>
-<p>©Ensemble Sound</p>`;
+<p><b>©Ensemble Sound</b></p>`;
